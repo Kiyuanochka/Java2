f067d106a1497a27284fb9bb40010bf519815541
Kiyuanochka <93634306+Kiyuanochka@users.noreply.github.com>
2022-09-13 19:34:52 +0700

    Initial commit

--- src/Lesson2/Main.java
+++ src/Lesson2/Main.java
@@ -1,24 +1,67 @@
 package Lesson2;
 
 public class Main {
+    static String[][] arraySizeException = {{"1", "2"}, {"3", "2"}, {"3", "4"}, {"3", "4"}};
+    static String[][] arrayDataException = {{"1", "2", "3", "u"}, {"3", "2", "3", "4"}, {"3", "4", "3", "8"}, {"6", "3", "4", "3"}};
+    static String[][] arrayWithNoError = {{"1", "2", "3", "8"}, {"3", "2", "3", "4"}, {"3", "4", "3", "8"}, {"6", "3", "4", "3"}};
 
+    static int sizeA = 4;
+    static int sizeB = 4;
 
-    public static void main(String[] args) throws MyArraySizeException{
-        MyTestClass myTestClass = new MyTestClass();
-       String[][] arr = new String[2][3];
-        myTestClass.test1();
+    public static void main(String[] args) {
+        try {
+            System.out.println( "Sum: " + getSumm(arrayWithNoError));}
+        catch (MyArraySizeException | MyArrayDataException ex) {
+            System.out.println(ex.getMessage());
 
+        }
+        try {
+            System.out.println( "Sum: " + getSumm(arraySizeException));}
+        catch (MyArraySizeException | MyArrayDataException ex) {
+            System.out.println(ex.getMessage());
 
-        //String[][] arr = new String[2][3];
+        }
+        try {
+            System.out.println( "Sum: " + getSumm(arrayDataException));}
+        catch (MyArraySizeException | MyArrayDataException ex) {
+            System.out.println(ex.getMessage());
 
-
-        //String[][] array = {{"1", "2", "3", "8"}, {"3", "2", "3", "4"}, {"3", "4", "3", "8"}, {"6", "3", "4", "3"}};
-
-
+        }
 
     }
 
+        static void checkValue (String[][]array) throws MyArraySizeException {
+            if (array.length != sizeA || array[0].length != sizeB) throw new MyArraySizeException();
+        }
 
-}
+        static Integer getSumm(String[][] array) throws MyArraySizeException, MyArrayDataException {
+            checkValue(array);
+            Integer result = 0;
+            int i = 0;
+            int j = 0;
+            try {
+                for (; i < array.length; i++) {
+                j = 0;
+                for (; j < array.length; j++) {
+                    result = result + Integer.parseInt(array[i][j]);
+                }}
+            } catch (NumberFormatException ex) {
+                throw new MyArrayDataException("Error in the position number " + i + "," + j);
+
+            }
+            return result;
+        }
+
+        public static class MyArraySizeException extends Exception {
+            MyArraySizeException() {
+                super("The size is no correct");
+            }
+        }
+        public static class MyArrayDataException extends Exception {
+            MyArrayDataException(String message) {
+                super(message);
+            }
+        }
 
 
+    }
--- src/Lesson2/MyArraySizeException.java
+++
@@ -1,9 +0,0 @@
-package Lesson2;
-
-public class MyArraySizeException extends Exception {
-
-    public MyArraySizeException(String s) {
-        super();
-    }
-
-}
